429b80dd68a960cee128fb0be764fbf3d76c04cb
ramit21 <21.ramit@gmail.com>
2017-08-12 20:24:44 +0530

    IE Driver

--- src/test/groovy/AbstractGebSpec.groovy
+++ src/test/groovy/AbstractGebSpec.groovy
@@ -1,5 +1,7 @@
 import geb.spock.GebSpec
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.ie.InternetExplorerDriver
+import org.openqa.selenium.remote.DesiredCapabilities
 
 //abstract class to ensure that this is not run as a standalone test case, even though name ends in *Spec
 abstract class AbstractGebSpec extends GebSpec { 
@@ -7,8 +9,14 @@ abstract class AbstractGebSpec extends GebSpec {
 	static def cachedDriver // static variable will store our single driver instance
 	
 	   def setupSpec() {
-		   if ( cachedDriver == null) { // in first Specification this will be true
-			   cachedDriver = new ChromeDriver() // so let's create driver and put it in cachedDriver variable
+		   if ( cachedDriver == null) { 
+			  /* System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+			   cachedDriver = new ChromeDriver() */
+			   DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+			   caps.setCapability("ignoreZoomSetting", true);
+			   System.setProperty("webdriver.ie.driver", "src/test/resources/IEDriverServer.exe");
+			   cachedDriver = new InternetExplorerDriver(caps)
+			   cachedDriver.get("http://www.yahoo.com");
 		   }
 	   }
 	
